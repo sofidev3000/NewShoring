15a0dbc182eb1eaaa4bd2e373713c24e66d5fb57
SofiDev <angiemeow89@gmail.com>
2024-08-29 15:48:27 -0600

    ✨ Carousel indicadores
    Se agrega carrusel infinito de indicadores financieros

--- src/components/atoms/Weather/WeatherPopover.jsx
+++ src/components/atoms/Weather/WeatherPopover.jsx
@@ -83,100 +83,89 @@ const formattedTime = currentTime.toLocaleTimeString("en-US", {
   hour12: true,
 });
 
-  return (
-    <>
-      <button
-        type="button"
-        className="btn btn-primary flex flex-wrap items-center gap-4 *:text-[14px]  *:text-white  relative "
-        onClick={handleWeatherButtonClick}
-      >
-        {/* Weather display */}
-        <div className="flex items-center gap-1 flex-wrap">
-          <img
-            className="weather-icon text-[--neutral-variant-color]   w-[29px]"
-            src={weatherData.iconImage}
-            alt="Weather Icon"
-          />
-          <span className="font-normal cursor-pointer text-white hover:text-[--guardsman-red-500]">
-            {weatherData.temperature}{" "}
-            <sup className="text-[9.2px] text-[--guardsman-red-800] mr-3">
-              c
-            </sup>{" "}
-            {weatherData.city}
+return (
+  <>
+    <button
+      type="button"
+      className="btn btn-primary flex flex-wrap items-center gap-4 *:text-[14px]  *:text-white  relative "
+      onClick={handleWeatherButtonClick}
+    >
+      {/* Weather display */}
+      <div className="flex items-center gap-1 flex-wrap">
+        <img
+          className="weather-icon text-[--neutral-variant-color]   w-[29px]"
+          src={weatherData.iconImage}
+          alt="Weather Icon"
+        />
+        <span className="font-normal cursor-pointer text-white hover:text-[--guardsman-red-500]">
+          {weatherData.temperature}{" "}
+          <sup className="text-[9.2px] text-[--guardsman-red-800] mr-3">
+            c
+          </sup>{" "}
+          {weatherData.city}
+        </span>
+      </div>
+
+      {/* Popover displaying weather details */}
+      {popoverOpen && (
+        <div
+          ref={popoverRef}
+          className="popover w-[11rem] bg-[--background-glass] backdrop-blur-[19px] shadow-lg p-3 *:text-[#cdcdcd] *:text-left rounded-lg absolute top-11 right-[-7px] z-30 flex-1"
+        >
+          <span className="flex">
+            <div className="w-full">
+              <div className="flex justify-center">
+                <img
+                  className="w-16 h-16"
+                  src={weatherData.iconImage}
+                  alt="Weather Icon"
+                />
+              </div>
+            </div>
           </span>
-        </div>
-        <button />
-
-        {/* Popover displaying weather details */}
-        {popoverOpen && (
-          <div
-            ref={popoverRef}
-            className="popover w-[11rem] bg-[--background-glass] backdrop-blur-[19px] shadow-lg p-3 *:text-[#cdcdcd] *:text-left rounded-lg absolute top-11 right-[-7px] z-30 flex-1"
-          >
-            <span className="flex">
-              <div className="w-full">
-                <div className="flex justify-center">
-                  <img
-                    className="w-16 h-16"
-                    src={weatherData.iconImage}
-                    alt="Weather Icon"
-                  />
-                </div>
-              </div>
-            </span>
-            <span className="flex w-full items-center text-2xl text-left font-bold">
-              {weatherData.temperature}
-              <sup className="m-0">c</sup>
-            </span>
-            <div className="flex my-2">
-              <div className="w-full">
-                <hr className="border-[--guardsman-red-800]" />
-              </div>
-            </div>
-            {/* Weather details */}
-            <div className="flex items-center gap-2">
-            <i className="fa-solid fa-location-dot"style={{color: "#ba0000"}}></i>
-              <span>Prob. Precip: {weatherData.precipProbability}</span>
-            </div>
-            <div className="flex items-center gap-2">
-            <i className="fa-solid fa-temperature-three-quarters"style={{color: "#ba0000"}}></i>
-              <span>Humedad: {weatherData.humidity}</span>
-            </div>
-            <div className="flex items-center gap-2">
-            <i className="fa-solid fa-droplet" style={{color: "#ba0000"}}></i>
-              <span>Viento: {weatherData.windspeed}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <i
-                className="fa-solid fa-umbrella"
-                style={{ color: "#ba0000" }}
-              ></i>
-              <span>
-                Temp-Max: {weatherData.temperatureMax}
-              </span>
-            </div>
-            <div className="flex items-center gap-2">
-              <i
-                className="fa-solid fa-temperature-arrow-down "
-                style={{ color: "#ba0000" }}
-              ></i>
-              <span>
-                Temp-Min: {weatherData.temperatureMin}
-
-              </span>
+          <span className="flex w-full items-center text-2xl text-left font-bold">
+            {weatherData.temperature}
+            <sup className="m-0">c</sup>
+          </span>
+          <div className="flex my-2">
+            <div className="w-full">
+              <hr className="border-[--guardsman-red-800]" />
             </div>
           </div>
-        )}
-      </button>
-      {/* Date and time display */}
-      <div className="flex items-center gap-2">
-        <time className="flex items-center cursor-pointer ">
-          {formattedDate}
-        </time>{" "}
-        - <time className="cursor-pointer">{formattedTime}</time>
-      </div>
-    </>
-  );
+          {/* Weather details */}
+          <div className="flex items-center gap-2">
+            <i className="fa-solid fa-location-dot" style={{ color: "#ba0000" }}></i>
+            <span>Prob. Precip: {weatherData.precipProbability}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="fa-solid fa-temperature-three-quarters" style={{ color: "#ba0000" }}></i>
+            <span>Humedad: {weatherData.humidity}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="fa-solid fa-droplet" style={{ color: "#ba0000" }}></i>
+            <span>Viento: {weatherData.windspeed}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="fa-solid fa-umbrella" style={{ color: "#ba0000" }}></i>
+            <span>Temp-Max: {weatherData.temperatureMax}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <i className="fa-solid fa-temperature-arrow-down" style={{ color: "#ba0000" }}></i>
+            <span>Temp-Min: {weatherData.temperatureMin}</span>
+          </div>
+        </div>
+      )}
+    </button>
+    {/* Date and time display */}
+    <div className="flex items-center gap-2">
+      <time className="flex items-center cursor-pointer ">
+        {formattedDate}
+      </time>{" "}
+      - <time className="cursor-pointer">{formattedTime}</time>
+    </div>
+  </>
+);
+
 };
 
 export default WeatherPopover;
